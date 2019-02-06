bc575bb25aa9d72410118f8e64e3110bbb48edec
Ahmet Sait <ahmetsaitkocak@gmail.com>
2019-02-06 03:58:52 +0300

    OpenGL loader formatting

--- src/blendui/graphics/gl/gl.d
+++ src/blendui/graphics/gl/gl.d
@@ -1,5 +1,8 @@
 module blendui.graphics.gl.gl;
 
-public import blendui.graphics.gl.types;
-public import blendui.graphics.gl.funcs;
-public import blendui.graphics.gl.enums;
+public 
+{
+	import blendui.graphics.gl.types;
+	import blendui.graphics.gl.funcs;
+	import blendui.graphics.gl.enums;
+}
