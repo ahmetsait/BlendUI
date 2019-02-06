d9bb52e3d1b90f70e21e4ed0a4567208446d12ed
Ahmet Sait <ahmetsaitkocak@gmail.com>
2019-02-06 05:21:46 +0300

    Add package.d for blendui.graphics

---
+++ src/blendui/graphics/package.d
@@ -0,0 +1,11 @@
+﻿module blendui.graphics;
+
+public
+{
+	import blendui.graphics.defaultshaders;
+	import blendui.graphics.font;
+	import blendui.graphics.gltype;
+	import blendui.graphics.mesh;
+	import blendui.graphics.shader;
+	import blendui.graphics.vertex;
+}
